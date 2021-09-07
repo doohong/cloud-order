8b980fc056107816386c83b13a3170967a81a554
doohong <wnghd94@gmail.com>
2021-09-08 01:12:18 +0900

    prometheus 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  // micrometer
+  implementation("io.micrometer:micrometer-registry-prometheus")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   // kafka
   implementation("org.springframework.kafka:spring-kafka")
